6a3bf3cfcb2eb42703e7472d01250f023bf62608
Žak Šilih <33088247+zsilih@users.noreply.github.com>
2020-09-27 00:03:03 +0200

    Added Cart Model

---
+++ MyShop/MyShop.Core/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Cart : BaseEntity
+    {
+        public virtual ICollection<CartItem> CartItems { get; set; }
+
+        public Cart()
+        {
+            this.CartItems = new List<CartItem>();
+        }
+    }
+}
